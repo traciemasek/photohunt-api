df93435ed0819eb87356fa8ad268dce6acc19c82
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-11-19 14:02:48 -0500

    dependency update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Score.destroy_all
-Score.create!([{
-  username: "fartyparty69", 
-  userscore: 69000
-},
+Score.destroy_all
+Score.create!([
+#   {
+#   username: "fartyparty69", 
+#   userscore: 69000
+# },
 {
   username: "yourMom", 
-  userscore: 69
+  userscore: 1000
 },
-{
-  username: "666", 
-  userscore: 666
-},
-{
-  username: "ruby", 
-  userscore: 52000
-},
-{
-  username: "karen", 
-  userscore: 900
-}
+# {
+#   username: "666", 
+#   userscore: 666
+# },
+# {
+#   username: "ruby", 
+#   userscore: 52000
+# },
+# {
+#   username: "karen", 
+#   userscore: 900
+# }
   
 ])
 
